928425205969684b40d6bcfc5271b3961a732dae
babtsoualiaksandr <babtsoualiaksandr@gmail.com>
2019-11-30 20:09:22 +0300

    fix: del concole.log

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ let items =  document.querySelector('.items');
 const URL = window.location;
 
 function showEducation(event) {
-  console.log(items)
-
   if (items.style.display === 'none') {
     items.style.display = 'block';
   } else {
@@ -12,19 +10,15 @@ function showEducation(event) {
 }
 document.querySelector('.education').addEventListener('click', showEducation);
 
-
 function jump(event) {
-  console.log(event);
   if (event.target.id === 'theyalow') {
     const url = 'http://'+URL.host + '/yalow/index.html';
     window.location.assign(url);    
   }
-
   if (event.target.id === 'repair') {
     const url = 'http://'+URL.host + '/repair/index.html';
     window.location.assign(url);
-  }
-  
+  }  
 }
 console.log(document.querySelector('.swiper-container'));
 document.querySelector('.swiper-container').addEventListener('click', jump);
