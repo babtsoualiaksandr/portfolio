d6076b74eee989b2a006973a452d7768bd0398e9
babtsoualiaksandr <babtsoualiaksandr@gmail.com>
2019-11-30 19:51:20 +0300

    fix: bugs

--- index.js
+++ index.js
@@ -1,51 +1,30 @@
 let items =  document.querySelector('.items');
 const URL = window.location;
 
-
 function showEducation(event) {
-  console.log(items.style)
+  console.log(items)
+
   if (items.style.display === 'none') {
-    console.log('display === none')
     items.style.display = 'block';
   } else {
-    console.log('display != none')
   items.style.display = 'none';
-  }
-  
+  }  
 }
-document.querySelector('.arrow').addEventListener('click', showEducation);
+document.querySelector('.education').addEventListener('click', showEducation);
 
 
 function jump(event) {
-  console.log(event)
-
+  console.log(event);
   if (event.target.id === 'theyalow') {
-    console.log('img#theyalow');
-    console.log(URL);
     const url = 'http://'+URL.host + '/TheYalow/index.html';
-    console.log(url);
-    window.location.assign(url);
-    
+    window.location.assign(url);    
   }
 
   if (event.target.id === 'repair') {
-    console.log('img#repair')
     const url = 'http://'+URL.host + '/repair-design-project/index.html';
-    console.log(url);
     window.location.assign(url);
   }
   
 }
-
-document.querySelector('#theyalow').addEventListener('click', jump);
-
-document.querySelector('#repair').addEventListener('click', jump);
-
-
-
-
-/* host: "127.0.0.1:5500"
-hostname: "127.0.0.1"
-href: "http://127.0.0.1:5500/index.html"
-origin: "http://127.0.0.1:5500"
-pathname: "/index.html" */
+console.log(document.querySelector('.swiper-container'));
+document.querySelector('.swiper-container').addEventListener('click', jump);
